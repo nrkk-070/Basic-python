fbe93a3732d75e78f17f5c18fc4956b103761d57
nrkk-070 <nrkk048@gmail.com>
2023-10-14 16:38:26 +0900

    制御構文の解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,12 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+n = input("nの値を入力: ")
+n = int(n)
 
-# TODO
+def jud(n):
+    if n <= 1:
+        print("nは素数でない")
+    for i in range(2, n):
+        if n % i == 0:
+            print("nは素数でない")
+            return
+    print("nは素数")
+jud(n)
